fefc0e708a5895fb9007acd2b9c4c1d51d622841
Yesua <65790207+YesuaPV@users.noreply.github.com>
2021-12-22 21:24:35 -0600

    j

--- src/pages/RFC.js
+++ src/pages/RFC.js
@@ -6,29 +6,38 @@ const RFC = ({
     onChange
 }) =>(
     <Fragment>
-        <div className="container d-flex justify-content-center mt-5">
-            <form className="form">
-                <div className="form-group mt-3">
-                    <label>Apellido paterno:</label>
-                    <input onChange={onChange} value={form.paterno} className="form-control" type="text" name="paterno" />
+        <div className="principal">
+        <div className="container-form">            
+            <div className="form-react">
+                <div className="text-center lbl-rfc mb-1">
+                    <h1>RFC</h1>
                 </div>
-                <div className="form-group mt-3">
-                    <label>Apellido materno:</label>
-                    <input onChange={onChange} value={form.materno} className="form-control" type="text" name="materno" />
+                <div className="container d-flex justify-content-center">
+                <form className="form">
+                    <div className="form-group mt-3">
+                        <label>Apellido paterno:</label>
+                        <input onChange={onChange} value={form.paterno} className="form-control" type="text" name="paterno" />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Apellido materno:</label>
+                        <input onChange={onChange} value={form.materno} className="form-control" type="text" name="materno" />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Nombre:</label>
+                        <input onChange={onChange} value={form.nombre} className="form-control" type="text" name="nombre" />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Fecha de nacimiento:</label>
+                        <input onChange={onChange} value={form.fechaNacimiento} className="form-control" type="date" name="fechaNacimiento" />
+                    </div>
+                </form>
                 </div>
-                <div className="form-group mt-3">
-                    <label>Nombre:</label>
-                    <input onChange={onChange} value={form.nombre} className="form-control" type="text" name="nombre" />
+                <div className="text-center mt-5">
+                    <h5 className="txt-rfc">{rfc}</h5>
                 </div>
-                <div className="form-group mt-3">
-                    <label>Fecha de nacimiento:</label>
-                    <input onChange={onChange} value={form.fechaNacimiento} className="form-control" type="date" name="fechaNacimiento" />
-                </div>
-            </form>
+            </div>
         </div>
-        <div className="text-center mt-5">
-            <h5 className="txt-rfc">{rfc}</h5>
-        </div>
+    </div>
     </Fragment>
 );
 
