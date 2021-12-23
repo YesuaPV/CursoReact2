38c4d72bb25d6073fc1cecc5f6490066172eaaea
Yesua <65790207+YesuaPV@users.noreply.github.com>
2021-12-22 19:14:47 -0600

    Prueba1

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './routes/App';
+import './styles/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//./node_modules/
+const container = document.getElementById('root');
+
+ReactDOM.render(<App/>,container);
---
+++ src/routes/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import IndexContainer from '../containers/IndexContainer';
+import WelcomeContainer from '../containers/WelcomeContainer';
+// import RFCContainer from '../containers/RFCContainer';
+// import ApiContainer from '../containers/ApiContainer';
+// import DomicilioContainer from '../containers/DomicilioContainer';
+
+const App = () =>{
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <IndexContainer />
+                </Route>
+                <Route exact path="/welcome">
+                    <WelcomeContainer />
+                </Route>
+                {/* <Route exact path="/rfc">
+                    <RFCContainer />
+                </Route>
+                <Route exact path="/api">
+                    <ApiContainer />
+                </Route>
+                <Route exact path="/domicilio">
+                    <DomicilioContainer />
+                </Route> */}
+                <Route>
+                    <div className="container mt-5 text-center">
+                        <h1>404 Not found</h1>
+                    </div>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default App;
